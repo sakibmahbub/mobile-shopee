9ee2c6f2b030f8148cbaaf0ffc0ca0233a74d6b9
sakibmahbub <arafatsakib48@gmail.com>
2020-12-26 01:15:05 +0600

    completed Home and Product page (Dynamic).

--- HTML Template/index.js
+++ HTML Template/index.js
@@ -71,28 +71,29 @@ $(document).ready(function () {
         }
     });
 
-    //Product qty section
+    // product qty section
     let $qty_up = $(".qty .qty-up");
     let $qty_down = $(".qty .qty-down");
-    let $input = $(".qty .qty_input");
+    // let $input = $(".qty .qty_input");
 
-    //Click on qty-up button
+    // click on qty up button
     $qty_up.click(function (e) {
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
         if ($input.val() >= 1 && $input.val() <= 9) {
             $input.val(function (i, oldval) {
                 return ++oldval;
-            })
+            });
         }
-
     });
 
-    //Click on qty-down button
+    // click on qty down button
     $qty_down.click(function (e) {
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
         if ($input.val() > 1 && $input.val() <= 10) {
             $input.val(function (i, oldval) {
                 return --oldval;
-            })
+            });
         }
-    })
+    });
 
 });
